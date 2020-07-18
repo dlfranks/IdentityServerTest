725551cba2501253fb2c6722ab22305fb8f1c8aa
Deana Franks <deana.franks@woodplc.com>
2020-07-18 09:15:50 -0400

    Initial commit

---
+++ microservice/microservice/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Authorize(Policy ="PublicSecure")]
+        [HttpGet("public")]
+        public ActionResult GetPublicData()
+        {
+            return Ok("This is public data.");
+        }
+
+        [Authorize(Policy = "UserSecure")]
+        [HttpGet("user")]
+        public ActionResult GetUserData()
+        {
+            return Ok("This is user data.");
+        }
+
+        [Authorize(Policy = "AdminSecure")]
+        [HttpGet("admin")]
+        public ActionResult UpdateAdminData()
+        {
+            return Ok("Admin has been updated data.");
+        }
+    }
+}
